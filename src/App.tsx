c6044858852af1e0669952729dad92fc2ae2b413
AkhilPolisetty <akhilpolisetty456@gmail.com>
2024-10-28 11:52:19 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import './App.css'
+import { Navbar } from "./components/navbar";
+import { Auth } from "./pages/auth";
+function App() {
+  
+
+  return (
+   <div>
+
+    <Router>
+      <Navbar/>
+       <Routes>
+
+        <Route path="/" element={<h1>Home</h1>}/>
+        <Route path="/auth" element={<Auth/>}/>
+        <Route path="/rated" element={<h1>Rated Page</h1>}/>
+       </Routes>
+
+    </Router>
+   </div>
+
+  );
+  
+      
+}
+
+export default App
---
+++ src/pages/auth/mutation.ts
@@ -0,0 +1,15 @@
+export const mutationLogin=async()=>{
+const res=await fetch(
+ "https://api.themoviedb.org/3/authentication/guest_session/new",
+  {
+
+    headers:{
+     Authorization:"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZjVkYTI4ZGU2NTI4NWEzNDU3MWM3MGUyYTg5NTg2NCIsIm5iZiI6MTcyOTc2MjY0Ni4xNjc5NzEsInN1YiI6IjY3MTc3YmVlNTBhNmViMGJmYmMyODE3OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.05CdhrIe2wwPwxEl8kI9gV0YYw-j4uZlq62uvLtOu4I"   
+
+    },
+ }   
+);
+console.log(res.json);
+return res.json();
+
+};
